5a7ae4c99369212ba2316ef960676d390a77f2e5
Boris Nadion <boris@astrails.com>
2016-10-26 09:29:49 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import './App.css';
 import Number from './components/number';
 
 class App extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = { counter: 0 };
+    this.onTimer = this.onTimer.bind(this);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.onTimer, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.onTimer);
+  }
+
+  onTimer() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
   render() {
+    console.log('rendering App');
     return (
       <div className="App">
         <div className="App-header">
           <h2>Hello world</h2>
         </div>
-        <p className="App-intro">
-          <Number value={ 12 } />
-        </p>
+        <div className="App-intro">
+          <Number value={ this.state.counter } />
+        </div>
       </div>
     );
   }
--- src/components/number.js
+++ src/components/number.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const Number = ({ value }) => (
-  <div>I'm a number { value }</div>
-);
+const Number = ({ value }) => {
+  console.log(`rendering number with ${value}`);
+  return (
+    <div>I'm a number { value }</div>
+  );
+};
 
 export default Number;
