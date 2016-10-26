39146059474e271b8bc1998497caacc9f38684e7
Boris Nadion <boris@astrails.com>
2016-10-26 13:03:05 +0300

    handleActions

--- src/redux/reducers/color.js
+++ src/redux/reducers/color.js
@@ -1,12 +1,10 @@
+import { handleActions } from 'redux-actions';
+
 const getColor = () => {
   const colors = [Math.random(), Math.random(), Math.random()];
   return `rgba(${colors.map(c => (c * 127).toFixed()).join(',')}, 1)`;
 };
 
-export const color = (state = 'rgba(0, 0, 0, 1)', action) => {
-  if (action.type === 'generate') {
-    return getColor();
-  }
-  return state;
-};
-
+export const color = handleActions({
+  'generate': () => getColor()
+}, 'rgba(0, 0, 0, 1)');
--- src/redux/reducers/counter.js
+++ src/redux/reducers/counter.js
@@ -1,6 +1,6 @@
-export const counter = (state = 0, action) => {
-  if (action.type === 'increment') {
-    return state + action.value;
-  }
-  return state;
-};
+import { handleActions } from 'redux-actions';
+
+export const counter = handleActions({
+  'increment': (state, action) => (state + action.value),
+  'decrement': (state, action) => (state - action.value)
+}, 0);
