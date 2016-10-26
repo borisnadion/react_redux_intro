bc09b65e911df4bae462edf1eb8493858068dbe5
Boris Nadion <boris@astrails.com>
2016-10-26 13:38:12 +0300

    move to container

--- src/components/number.js
+++ src/components/number.js
@@ -1,15 +1,5 @@
 import React from 'react';
 
-const convertNumber = (number) => {
-  let res = number;
-  if (number % 2) {
-    res += " even";
-  } else {
-    res += " odd";
-  }
-  return res;
-};
-
 const Number = ({ changeColor, number, color }) => {
   const onChangeColor = (e) => {
     e.preventDefault();
@@ -18,11 +8,9 @@ const Number = ({ changeColor, number, color }) => {
 
   console.log('  rendering number with', number, color);
 
-  const value = convertNumber(number);
-
   return (
     <div>
-      <div style={ {color: color} }>I'm a number: { value }</div>
+      <div style={ {color: color} }>I'm a number: { number }</div>
       <a href="#" onClick={ onChangeColor }>Color</a>
     </div>
   );
--- src/containers/main.js
+++ src/containers/main.js
@@ -2,11 +2,22 @@ import { connect } from 'react-redux';
 import Main from '../components/main';
 import { increment } from '../redux/actions/increment';
 
+const convertNumber = (number) => {
+  let res = number;
+  if (number % 2) {
+    res += " even";
+  } else {
+    res += " odd";
+  }
+  return res;
+};
+
+
 const mapDispatchToProps = (dispatch) => ({
   increment: (value) => dispatch(increment(value))
 });
 
-const mapStateToProps = (state) => ({ counter: state.counter });
+const mapStateToProps = (state) => ({ counter: convertNumber(state.counter) });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
 
